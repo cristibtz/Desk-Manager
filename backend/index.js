3eaae3908138b4f9061b5ab491f5aefe5f116db2
Cristian Branet <gbranet@dvloper.io>
2025-01-19 14:12:29 +0000

    Working on API endpoints. Added some admin endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const db = require('./database/database.js');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express()
 const port = 3000
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(adminRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
