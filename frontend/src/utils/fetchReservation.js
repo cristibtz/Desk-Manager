b06ef8bd71f473fe5b7f5c416557c67f447f3a94
cristibtz <cristix231@gmail.com>
2025-02-19 21:01:11 +0200

    Another small modification

--- frontend/src/utils/fetchReservation.js
+++ frontend/src/utils/fetchReservation.js
@@ -3,5 +3,5 @@ import { createApiClient } from './apiClient';
 export const fetchReservation = async (token, id) => {
         const apiClient = createApiClient(token);
         const reservationData = await apiClient.get(`/user/reservations/${id}`);
-        reservationData.data;
+        return reservationData.data;
 };
