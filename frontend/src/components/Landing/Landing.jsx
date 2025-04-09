08a077c3f895a1a22b196b834191bab9c57fab73
Cristian Branet <gbranet@dvloper.io>
2025-04-09 14:11:43 +0000

    Pipeline works

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -3,12 +3,14 @@ import React from "react";
 function Landing() {
 
   return (
-    <h1 className="text-center text-4xl">
-      Hello!
-    </h1>
-    <h2 className="text-center text-4xl">
-      This is a webb application that allows you to book desks.
-    </h2>
+    <div>
+      <h1 className="text-center text-4xl">
+        Hello!
+      </h1>
+      <h2 className="text-center text-4xl">
+        This is a webb application that allows you to book desks.
+      </h2>
+    </div>
   );
 }
 
