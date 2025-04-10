b594b51d34ce03b49e89363c9c6b68bb2cf7c698
Cristian Branet <gbranet@dvloper.io>
2025-04-10 18:59:32 +0000

    small change

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ function Landing() {
       </h1>
       <h2 className="text-center text-4xl">
         This is a web application that allows you to book desks. 
-          <a href="/dashboard">
+          <a href="/dashboard" className="underline text-blue-500 hover:text-blue-800">
             Proceed!
           </a>
       </h2>
